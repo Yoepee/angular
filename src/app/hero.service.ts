13a22e0b949c9325e9c02d37eeee7be2a3a3bf04
Yoepee <kdy2975@naver.com>
2023-01-04 19:25:45 +0900

    히어로 수정, 삭제 추가

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -38,13 +38,35 @@ export class HeroService {
     )
   }
 
-  // updateHero(hero: Hero): Observable<Hero> {
-  //   return this.http.put(this.heroesUrl, hero, this.httpOptions)
-  //   .pipe(
-  //     tap(_=> this.log(`updated hero id = ${hero.id}`)),
-  //     catchError(this.handleError<any>('updateHero'))
-  //   );
-  // }
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions)
+    .pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  /** POST: add a new hero to ther server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions)
+    .pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>(`addHero`))
+      );
+  }
+
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_=> this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
+  httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   // getHeroes():Hero[]{
   //   return HEROES;
