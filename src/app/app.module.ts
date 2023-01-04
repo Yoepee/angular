0879e9e11d1bc4b202fb983cc6452752770784a9
dykim2975 <dykim@me-zoo.com>
2023-01-04 17:42:27 +0900

    추가 : 데이터베이스 외부 데이터 받아오기 api 지정

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
@@ -20,7 +24,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    // HttpClientInMemoryWebApiModule 모듈은 HTTP 요청을 가로채고 서버의 응답을 흉내냅니다.
+    // 실제 서버가 준비되면 이 부분을 제거하면 됩니다.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,22 +6,67 @@ import { Observable, of } from 'rxjs';
 
 import { MessageService } from './message.service';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { catchError, map, tap } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-  getHeroes(): Observable<Hero[]>{
-    const heroes = of(HEROES);
-    this.messageService.add("HeroService: fetched heroes")
-    return heroes;
+  private heroesUrl = 'api/heroes';
+
+  getHeroes(): Observable<Hero[]> {
+    // const heroes = of(HEROES);
+    // this.messageService.add("HeroService: fetched heroes")
+    // return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_=> this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      )
   }
-  getHero(id: number): Observable<Hero>{
-    const hero = HEROES.find(h => h.id === id)!;
-    this.messageService.add("HeroService: fetched heroes")
-    return of(hero);
+  getHero(id: number): Observable<Hero> {
+    // const hero = HEROES.find(h => h.id === id)!;
+    // this.messageService.add("HeroService: fetched heroes")
+    // return of(hero);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url)
+    .pipe(
+      tap(_=> this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id = ${id}`))
+    )
   }
   // getHeroes():Hero[]{
   //   return HEROES;
   // }
-  constructor(private messageService: MessageService) { }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  /**
+ * HTTP 요청이 실패한 경우를 처리합니다.
+ * 애플리케이션 로직 흐름은 그대로 유지됩니다.
+ *
+ * @param operation - 실패한 동작의 이름
+ * @param result - 기본값으로 반환할 객체
+ */
+  private handleError<T>(operation = 'operation', result?: T){
+    return (error:any) : Observable<T> => {
+      // TODO: 리모트 서버로 에러 메시지 보내기
+      console.error(error); // 지금 콘솔에 로그를 출력
+
+      // TODO: 사용자가 이해할 수 있는 형태로 변환하기
+      this.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T)
+    }
+  }
+  
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) { }
 }
